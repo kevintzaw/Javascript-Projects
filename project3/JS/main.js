cdd4f6f9b928f833ad15c9c683f380abbf575395
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2021-12-30 20:21:43 -0500

    project 3

--- project3/JS/main.js
+++ project3/JS/main.js
@@ -1,7 +1,29 @@
-function creatingWords(){
-    document.write("You just created text!");
+//Adding//
+function addNum(p1,p2){
+    return p1 + p2;
+}
+//Subtraction//
+function subNum(p1,p2){
+    return p1 - p2;
+}
+//Multiply//
+function mulNum(p1,p2){
+    return p1 * p2;
+}
+//Remainder//
+function remNum(p1,p2){
+    return p1 % p2;
+}
+//Add More//
+function moreNum(p1){
+    return ++p1;
+}
+//Less//
+function lessNum(p1){
+    return --p1;
 }
 
-function creatingAlert(){
-    window.alert("You just created an alert!")
+//Writing Hello//
+function creatingWords(){
+    document.write("You just created text!");
 }
