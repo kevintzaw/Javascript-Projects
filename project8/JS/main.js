f5c6ec265894c663c093e927a04a49e70fb2f3dc
kevintzaw <88251025+kevintzaw@users.noreply.github.com>
2022-01-02 18:20:30 -0500

    project 8

---
+++ project8/JS/main.js
@@ -0,0 +1,12 @@
+// Variables for Slice and Concat //
+const arr1 = ["Thor", "Hulk"];
+const arr2 = ["Ironman", "Captain America", "Spiderman"];
+const avengersAll = arr1.concat(arr2);
+const avengersSome = arr2.slice(2);
+
+// Variables for Text to String //
+var textToString = "This will be a String";
+var becameString = textToString.toString();
+
+// Variables for Precision //
+var numDecimal = 13.3714;
